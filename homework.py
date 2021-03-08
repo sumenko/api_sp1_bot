30b32ccf2795fd4be42d7401b9831aa93dd135c1
Vladimir Sumenko <3760097@gmail.com>
2021-03-08 13:46:31 +0300

    all works

--- homework.py
+++ homework.py
@@ -1,13 +1,10 @@
+import logging
 import os
 import time
-import datetime
 
-import logging
 import requests
 import telegram
 from dotenv import load_dotenv
-from pprint import pprint
-
 
 load_dotenv()
 
@@ -19,58 +16,70 @@ yabot = telegram.Bot(TELEGRAM_TOKEN)
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s %(levelname)s %(lineno)s %(message)s")
 log = logging.getLogger('__name__')
-"""
-Бот должен логировать момент своего запуска (уровень DEBUG),
-каждую отправку сообщения (уровень INFO) и отправлять сообщения
-уровня ERROR вам в Телеграм.
-"""
+
+
 def parse_homework_status(homework):
     homework_name = homework['homework_name']
     if homework['status'] == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
-        verdict = ('Ревьюеру всё понравилось, можно'
+        verdict = ('Ревьюеру всё понравилось, можно '
                    'приступать к следующему уроку.')
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
-# Вход
+
 def get_homework_statuses(current_timestamp):
     url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
-    
-    homework_statuses = requests.get(url, 
-                                     headers={
-                                              'Authorization':
-                                              f'OAuth {PRAKTIKUM_TOKEN}'
-                                             },
-                                     params={'from_date': current_timestamp})
-    return homework_statuses.json()
-    
 
-def send_message(message, bot_client):
-    log.info('send message: ' + message)
+    homework_statuses = requests.get(url,
+                                     headers={
+                                         'Authorization':
+                                         f'OAuth {PRAKTIKUM_TOKEN}'},
+                                     params={'from_date': current_timestamp})
+    json = homework_statuses.json()
+    if 'message' in json:
+        err_msg = "API error: " + json.get('message')
+        log.error(err_msg)
+        send_message(err_msg)
+    return json
+
+
+def send_message(message, bot_client=None):
+    if not bot_client:
+        bot_client = yabot
     return bot_client.send_message(CHAT_ID, message)
 
 
 def main():
     # проинициализировать бота здесь
     current_timestamp = int(time.time())  # начальное значение timestamp
-    log.debug('Bot start watching')
-    send_message('Bot start watching', yabot)
+    log.debug('Start watching homework')
+    send_message('Start watching...')
+
     while True:
         try:
+            # смотрим статус домашки начиная с текущего момента
             new_homework = get_homework_statuses(current_timestamp)
+
             if new_homework.get('homeworks'):
                 send_message(parse_homework_status(
                     new_homework.get('homeworks')[0]), yabot)
-            
-            # обновить timestamp
-            current_timestamp = new_homework.get('current_date', 
-                                                 current_timestamp)  
-            # time.sleep(300)  # опрашивать раз в пять минут
-            time.sleep(5)  # опрашивать раз в пять минут
+
+            if 'error' in new_homework:
+                msg_err = 'API error: {}'.format(
+                    new_homework.get('error').get('error'))
+                log.error(msg_err)
+                send_message(msg_err)
+
+            # обновить timestamp, поскольку до этого момента данные уже есть
+            current_timestamp = new_homework.get('current_date',
+                                                 current_timestamp)
+            time.sleep(300)  # опрашивать раз в пять минут
 
         except Exception as e:
-            log.error(f'Бот столкнулся с ошибкой: {e}')
+            err_msg = f'Бот столкнулся с ошибкой: {e}'
+            log.error(err_msg)
+            send_message(err_msg)
 
             time.sleep(5)
 
