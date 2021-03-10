b58e537abd08b5ca05564cd3febf312ba8684e84
Vladimir Sumenko <3760097@gmail.com>
2021-03-10 09:52:28 +0300

    review 1 fixes

--- homework.py
+++ homework.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 import os
 import time
@@ -8,7 +9,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-PRAKTIKUM_TOKEN = os.environ.get("PRAKTIKUM_TOKEN")
+PRAKTIKUM_TOKEN = os.environ.get('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
 CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
 
@@ -21,43 +22,72 @@ log = logging.getLogger('__name__')
 
 
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']
-    if homework['status'] == 'reviewing':
-        return 'Работа взята в ревью'
-    if homework['status'] == 'rejected':
-        verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
-        verdict = ('Ревьюеру всё понравилось, можно '
-                   'приступать к следующему уроку.')
-
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    statuses = {
+        'reviewing': 'Работа взята в ревью',
+        'rejected': 'К сожалению в работе нашлись ошибки.',
+        'approved': ('Ревьюеру всё понравилось, можно '
+                     'приступать к следующему уроку.'),
+    }
+    try:
+        homework_name = homework['homework_name']
+        verdict = statuses[homework['status']]
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    except KeyError:
+        return 'Неверный ответ сервера'
 
 
 def get_homework_statuses(current_timestamp):
     url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+    if current_timestamp is None:
+        current_timestamp = get_UTC_stamp()
 
-    homework_statuses = requests.get(url,
-                                     headers={
-                                         'Authorization':
-                                         f'OAuth {PRAKTIKUM_TOKEN}'},
-                                     params={'from_date': current_timestamp})
+    try:
+        homework_statuses = requests.get(url,
+                                         headers={
+                                             'Authorization':
+                                             f'OAuth {PRAKTIKUM_TOKEN}'},
+                                         params={'from_date':
+                                                 current_timestamp})
+    except requests.RequestException as e:  # не уверен что этот нужен
+        error_report(f'При выполнении запроса произолша ошибка: {e}')
+        return None
+    except requests.HTTPError as e:
+        error_report(f'При выполнении запроса произолша ошибка: {e}')
+        return None
+
     json = homework_statuses.json()
     if 'message' in json:
-        err_msg = "API error: " + json.get('message') + json
-        log.error(err_msg)
-        send_message(err_msg)
+        error_report('API error: ' + json.get('message') + json)
+    log.debug(json)
     return json
 
 
 def send_message(message, bot_client=None):
+    """ Отправляет сообщение от бота по умолчанию или переданного """
     if not bot_client:
         bot_client = yabot
     return bot_client.send_message(CHAT_ID, message)
 
 
+def error_report(message):
+    """ Отправляет сообщение об ошибке в нужные места """
+    log.error(message)
+    send_message(message)
+
+
+def get_UTC_stamp(year=None, month=None, day=None):
+    """ отдает timestamp в UTC, можно подставить свое для дебага """
+    if not (year and month and day):
+        return int(time.time())
+
+    dt = datetime.date(year, month, day)
+    return int(time.mktime(dt.timetuple()))
+
+
 def main():
     # проинициализировать бота здесь
-    current_timestamp = int(time.time())  # начальное значение timestamp
+    current_timestamp = get_UTC_stamp()  # начальное значение timestamp
+
     log.debug('Start watching homework')
     send_message('Start watching...')
     while True:
@@ -72,8 +102,7 @@ def main():
             if 'error' in new_homework:
                 msg_err = 'API error: {}'.format(
                     new_homework.get('error').get('error'))
-                log.error(msg_err)
-                send_message(msg_err)
+                error_report(msg_err)
 
             # обновить timestamp, поскольку до этого момента данные уже есть
             current_timestamp = new_homework.get('current_date',
@@ -82,8 +111,7 @@ def main():
 
         except Exception as e:
             err_msg = f'Бот столкнулся с ошибкой: {e}'
-            log.error(err_msg)
-            send_message(err_msg)
+            error_report(err_msg)
             time.sleep(60)
 
 
